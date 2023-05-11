58f7ce148b66214c01421d37714479c2e4f87e41
CskaiPeti <bacskaipeter01@gmail.com>
2023-05-11 23:48:27 +0200

    kesz

---
+++ tesztek.js
@@ -0,0 +1,99 @@
+function checkAnswers() {
+    const form = document.getElementById('quizForm');
+    const result = document.getElementById('result');
+
+    
+    const inputs = form.querySelectorAll('input, select, textarea');
+    let isComplete = true;
+    for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].value === '') {
+            isComplete = false;
+            break;
+        }
+    }
+
+    if (isComplete) {
+        // Válaszok ellenőrzése
+        const answer1 = form.querySelector('input[name="kerdes1"]:checked');
+        const answer2 = form.querySelectorAll('input[name="kerdes2"]:checked');
+        const answer3 = form
+            .querySelector('input[name="kerdes3"]')
+            .value
+            .toLowerCase();
+        const answer4 = form
+            .querySelector('textarea[name="kerdes4"]')
+            .value;
+        const answer5 = form
+            .querySelector('select[name="kerdes5"]')
+            .value;
+        const answer5_2 = form
+            .querySelector('select[name="kerdes5.1"]')
+            .value;
+
+        let score = 0;
+
+        // 1. kérdés ellenőrzése
+        if (answer1 && answer1.value === 'A') {
+            score += 1;
+        }
+
+        // 2. kérdés ellenőrzése
+        if (answer2 && answer2.length >= 2) {
+            let hasA = false;
+            let hasB = false;
+
+            for (let i = 0; i < answer2.length; i++) {
+                if (answer2[i].value === 'A') {
+                    hasA = true;
+                } else if (answer2[i].value === 'B') {
+                    hasB = true;
+                }
+            }
+
+            if (hasA && hasB) {
+                score += 1;
+            }
+        }
+
+        // 3. kérdés ellenőrzése
+        if (answer3 === 'let') {
+            score += 1;
+        }
+
+        // 4. kérdés ellenőrzése
+        const keywords = ['var', 'let', 'const', 'function', 'class'];
+        let count = 0;
+
+        for (let i = 0; i < keywords.length; i++) {
+            if (answer4.includes(keywords[i])) {
+                count += 1;
+                if (count >= 3) {
+                    break;
+                }
+            }
+        }
+
+        const isAnswer4Correct = count >= 3;
+
+        if (isAnswer4Correct) {
+            score += 1;
+        }
+
+        // 5. kérdés ellenőrzése
+        if (answer5 === '0' && answer5_2 === '2') {
+            score += 1;
+        }
+        const audio = document.getElementById('Zene');
+        
+
+        // Eredmény kiszámítása és megjelenítése
+        const percentage = (score / 5) * 100;
+        const resultElement = document.getElementById('result');
+        result.textContent = `Eredmény: ${percentage}%`;
+        if (percentage===100) {
+            audio.play();
+        }
+    } else {
+        result.textContent = 'Kérlek tölts ki minden mezőt!';
+    }
+}
---
+++ urlap.js
@@ -0,0 +1,110 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const birthdate = document.getElementById('birthdate');
+
+const acceptedPassword = 'jelszo'; // Előre megadott rettentően kreatív jelszó
+
+// Hibaüzenetek megjelenítése
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+
+}
+
+// Sikeres kitöltés jelzése
+function showSucces(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+// Felhasználónév formátum ellenőrzése
+function checkUsername(input) {
+    const re = /^[A-Z][a-zA-Z]* [A-Z][a-zA-Z]*$/;
+    if (re.test(input.value.trim())) {
+        showSucces(input);
+    } else {
+        showError(input, 'A felhasználónevet helytelenül adta meg.');
+    }
+}
+
+// Email formátum ellenőrzése
+function checkEmail(input) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (re.test(input.value.trim())) {
+        showSucces(input);
+    } else {
+        showError(input, 'Az email cím érvénytelen');
+    }
+}
+
+// Születési dátum formátum ellenőrzése
+function checkBirthdate(input) {
+    const re = /^(19[0-9]{2}|20[0-2]{2}|2023)-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
+    if (re.test(input.value.trim())) {
+        showSucces(input);
+    } else {
+        showError(input, 'Érvénytelen születési dátum. (ÉÉÉÉ-HH-NN)');
+    }
+}
+
+// Kitöltési kötelezettség ellenőrzése
+function checkRequired(inputArr) {
+    inputArr.forEach(function (input) {
+        if (input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} kitöltése kötelező`);
+        } else {
+            showSucces(input);
+        }
+    });
+}
+
+// Bemeneti mező hosszának ellenőrzése
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(
+            input,
+            `${getFieldName(input)} legalább ${min} karakter hosszú kell legyen`
+        );
+    } else if (input.value.length > max) {
+        showError(
+            input,
+            `${getFieldName(input)} legfeljebb ${max} karakter hosszú lehet`
+        );
+    } else {
+        showSucces(input);
+    }
+}
+
+//get FieldName
+function getFieldName(input) {
+    return input
+        .id
+        .charAt(0)
+        .toUpperCase() + input
+        .id
+        .slice(1);
+}
+
+//Event Listeners
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    checkRequired([username, email, password]);
+    checkLength(username, 2, 15);
+    checkEmail(email);
+    checkBirthdate(birthdate);
+
+    if (password.value === acceptedPassword) {
+        showSucces(password);
+    } else {
+        showError(password, 'Invalid password');
+    }
+    checkUsername(username);
+    if (!username.parentElement.classList.contains('error') && !email.parentElement.classList.contains('error') && !password.parentElement.classList.contains('error') && !birthdate.parentElement.classList.contains('error')) {
+        window.location.href = 'urlap.html';
+    }
+});
